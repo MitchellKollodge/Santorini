7b60e6c8b572889ab387639c20f00c25e7303f94
MKollodge-GeoComm <43356584+MKollodge-GeoComm@users.noreply.github.com>
2019-01-01 21:48:35 -0600

    Added a lot. In the process of improving the AI.

---
+++ node.py
@@ -0,0 +1,9 @@
+
+
+class Node:
+    def __init__(self):
+        self.parentNode = None
+        self.leafNode = False
+        self.childNodes = []
+        self.value = None
+        self.actionSet = None
